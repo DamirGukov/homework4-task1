9673472bbfa661de7f7197f748a6b01ff58a31a8
DamirGukov <122615875+DamirGukov@users.noreply.github.com>
2023-06-17 12:38:24 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func searchText(textSaver []string) {
 	var search string
 	found := false
 
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Println("Find your text:")
-		fmt.Scan(&search)
+		scanner.Scan()
+		search = scanner.Text()
 
 		for _, value := range textSaver {
-			if value != "" && value == search {
+			if strings.Contains(value, search) {
 				fmt.Println(value)
 				found = true
 			}
@@ -21,6 +29,7 @@ func searchText(textSaver []string) {
 			break
 		}
 	}
+
 	if !found {
 		fmt.Println("No matches found.")
 	}
@@ -30,9 +39,12 @@ func main() {
 	textSaver := make([]string, 0)
 	var text string
 
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Println("Enter text (or 'continue' to continue): ")
-		fmt.Scan(&text)
+		scanner.Scan()
+		text = scanner.Text()
 
 		if text == "continue" {
 			break
